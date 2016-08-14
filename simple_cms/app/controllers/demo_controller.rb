218c81c59755cb9d55e58ea04d1b5d4bbeeded3d
John <johnconley507@hotmail.com>
2016-08-13 21:17:17 -0400

    adding links to view pages

--- simple_cms/app/controllers/demo_controller.rb
+++ simple_cms/app/controllers/demo_controller.rb
@@ -4,11 +4,13 @@ class DemoController < ApplicationController
 
 
   def index
-  	render "hello"
   end
 
   def hello
-	render "index"
+	# render "index"
+	@array = [1,2,3,4,5]
+	@id = params['id'].to_i
+	@page = params[:page].to_i
   end
 
   def other_hello
