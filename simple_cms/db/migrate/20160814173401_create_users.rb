b5f3901b7fe6367a750c1098d67149a6d3d5eb28
John <johnconley507@hotmail.com>
2016-08-14 13:41:32 -0400

    correcting migration

--- simple_cms/db/migrate/20160814173401_create_users.rb
+++ simple_cms/db/migrate/20160814173401_create_users.rb
@@ -2,6 +2,13 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def up
     create_table :users do |t|
       t.column "first_name", :string
+      t.string "last_name", :limit => 50
+      t.string "email", :default => "", :null => false
+      t.string "password", :limit => 40
+      
+      # t.datetime "created_at"
+      # t.datetime "updated_at"
+      
       t.timestamps
     end
   end
@@ -9,4 +16,5 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def down
   	drop_table :users
   end
+
 end
