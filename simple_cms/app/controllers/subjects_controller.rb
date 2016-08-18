0411dbb9f5890ff48f993493b781e76615e6493b
John <johnconley507@hotmail.com>
2016-08-18 14:32:02 -0400

    adding new create forms

--- simple_cms/app/controllers/subjects_controller.rb
+++ simple_cms/app/controllers/subjects_controller.rb
@@ -11,6 +11,16 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new({:name => 'Default'})
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
@@ -18,4 +28,10 @@ class SubjectsController < ApplicationController
 
   def delete
   end
+
+  private
+
+    def subject_params
+      params.require(:subject).permit(:name, :position, :visible)
+    end
 end
