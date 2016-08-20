da57c80616c4a8c0d6df31fba55b690330e67028
John <johnconley507@hotmail.com>
2016-08-19 23:48:43 -0400

    generating Pages and Section controller

--- simple_cms/config/routes.rb
+++ simple_cms/config/routes.rb
@@ -1,5 +1,53 @@
 Rails.application.routes.draw do
   
+  get 'section/index'
+
+  get 'section/show'
+
+  get 'section/new'
+
+  get 'section/create'
+
+  get 'section/edit'
+
+  get 'section/update'
+
+  get 'section/delete'
+
+  get 'section/destroy'
+
+  get 'pages/index'
+
+  get 'pages/show'
+
+  get 'pages/new'
+
+  get 'pages/create'
+
+  get 'pages/edit'
+
+  get 'pages/update'
+
+  get 'pages/delete'
+
+  get 'pages/destroy'
+
+  get 'page/index'
+
+  get 'page/show'
+
+  get 'page/new'
+
+  get 'page/create'
+
+  get 'page/edit'
+
+  get 'page/update'
+
+  get 'page/delete'
+
+  get 'page/destroy'
+
   get 'demo/index'
   match ':controller(/:action(/:id))', :via => :get
   root 'demo#index'
