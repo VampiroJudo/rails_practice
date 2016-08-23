1fe22507006fef8704344e8d23355c561534b355
John <johnconley507@hotmail.com>
2016-08-23 00:09:36 -0400

    updating models

--- simple_cms/app/models/page.rb
+++ simple_cms/app/models/page.rb
@@ -1,5 +1,11 @@
 class Page < ApplicationRecord
 	belongs_to :subject
-	# has_and_belongs_to_many :editors, :class_name "AdminUser"
+	has_many :sections
+	has_and_belongs_to_many :editors, :class_name => 'AdminUser'
+
+	scope :visible, lamba { where(:visible => true) }
+	scope :invisible, lamba { where(:visible => false) }
+	scope :sorted, lamba { order("pages.position ASC") }
+	scope :newest_first, lamba { order('pages.created_at DESC') }
 	
 end
--- simple_cms/app/models/section.rb
+++ simple_cms/app/models/section.rb
@@ -1,5 +1,12 @@
 class Section < ApplicationRecord
+	belongs_to :page
 	has_many :section_edits
-	has_many :admin_users, :through => :section_edits, :class_name
+	has_many :editors, :through => :section_edits, :class_name
 	=> "AdminUser"
+
+	scope :visible, lamba { where(:visible => true) }
+	scope :invisible, lamba { where(:visible => false) }
+	scope :sorted, lamba { order("sections.position ASC") }
+	scope :newest_first, lamba { order('sections.created_at DESC') }
+	
 end
