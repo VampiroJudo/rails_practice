214c8b9431986661d129c371cf7385aa53335915
John <johnconley507@hotmail.com>
2016-08-23 00:03:40 -0400

    updating Sections Controller

--- simple_cms/app/controllers/sections_controller.rb
+++ simple_cms/app/controllers/sections_controller.rb
@@ -1,26 +1,56 @@
 class SectionsController < ApplicationController
-  def index
-    @sections = Section.sorted
+  ddef index
+    @section = Section.sorted
   end
 
   def show
+    @section= Section.find(params[:id])
   end
 
   def new
-  end
-
-  def create
+    @section= Section.new({:name => "Default"})
   end
 
   def edit
+    @section= Section.find(params[:id])
+    if @sectionupdate_attribute(section_params)
+      flash[:notice] = "Section updated succesfully."
+      redirect_to(:action => 'show', :id => @sectionid)
+    else
+      render('edit')
+    end
+  end
+
+  def create
+    @section= Section.new({:name => 'Default'})
+    if @sectionsave
+      flash[:notice] = 'Section created successfully.'
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end    
   end
 
   def update
+    @section= Section.find(params[:id])
+    if @sectionupdate_attribute(section_params)
+      flash[:notice] = 'Section updated successfully.'
+      redirect_to(:action => 'show', :id => @sectionid)
+    else
+      render('edit')
+    
   end
 
   def delete
+    @section= Section.find(params[:id])
   end
 
   def destroy
+    section= Section.find(params[:id]).destroy
+    flash[:notice] = "Section destroyed successfully."
+    redirect_to(:action => 'index')
+    
   end
+  private
+
 end
